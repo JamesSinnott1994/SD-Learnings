b4cf9c856d2837a57d8a821b115d049445c99376
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-07 19:42:55 +0100

    [ADD] Multiple inheritance through interfaces

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -1,7 +1,33 @@
+interface one {
+    void print_geek();
+}
+interface two {
+    void print_for();
+}
+
+interface three extends one, two {
+    void print_geek();
+}
+
+class child implements three {
+    @Override public void print_geek() {
+        System.out.println("Geeks");
+    }
+
+    public void print_for() { System.out.println("for"); }
+}
+
 public class Main {
 
-    public static void main(String args[]) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb);
+
+        child c = new child();
+        c.print_geek();
+        c.print_for();
+        c.print_geek();
     }
 
 }
