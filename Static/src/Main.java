7ce78760d8fcbcfe58fab173f268cc6d6acc3452
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-08 19:06:55 +0100

    [ADD] Simple program to demonstrate static member being accessed before instantiating a clas

--- Static/src/Main.java
+++ Static/src/Main.java
@@ -2,8 +2,16 @@ package src;
 
 public class Main {
 
-    public static void main(String[] args) {
+    // Java program to demonstrate that a static member
+    // can be accessed before instantiating a class
+    static void m1() {
+        System.out.println("From m1");
+    }
 
+    public static void main(String[] args) {
+        // calling m1 without creating
+        // any object of class Main
+        m1();
     }
 
 }
