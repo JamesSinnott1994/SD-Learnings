93749ff7cc477d2d185d70e1e3549591d3366113
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-18 16:27:37 +0100

    [ADD] Concrete "Circle" class added

---
+++ Abstraction/src/ie/sinnott/Circle.java
@@ -0,0 +1,22 @@
+package ie.sinnott;
+
+public class Circle extends Shape{
+
+    double radius;
+
+    public Circle(String colour, double radius) {
+        // Calling Shape constructor
+        super(colour);
+        System.out.println("Circle constructor called");
+        this.radius = radius;
+    }
+
+    @Override double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override public String toString() {
+        return "Circle colour is " + super.getColour() + "and area is : " + area();
+    }
+
+}
