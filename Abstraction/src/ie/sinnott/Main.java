e4acaaeb1e0b15f3b03f9ebae51578fdac41a0c5
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-18 16:56:07 +0100

    [ADD] Two Animal objects of type Cat and Dog created with output

--- Abstraction/src/ie/sinnott/Main.java
+++ Abstraction/src/ie/sinnott/Main.java
@@ -1,5 +1,8 @@
 package ie.sinnott;
 
+import ie.sinnott.animal.Animal;
+import ie.sinnott.animal.Cat;
+import ie.sinnott.animal.Dog;
 import ie.sinnott.shape.Circle;
 import ie.sinnott.shape.Rectangle;
 import ie.sinnott.shape.Shape;
@@ -8,11 +11,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Shape example
         Shape s1 = new Circle("Red", 2.2);
         Shape s2 = new Rectangle("Yellow", 2, 4);
 
         System.out.println(s1.toString());
         System.out.println(s2.toString());
+
+        System.out.println();
+        System.out.println("*************************************");
+        System.out.println();
+
+        // Animal example
+        Animal myDog = new Dog("Bruno");
+        Animal myCat = new Cat("Friendly");
+
+        myDog.makeSound();
+        myCat.makeSound();
 
     }
 }
