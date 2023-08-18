3ee8f4ac887fa3d3f87c1f87ebf5a4f31e2c6c47
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-18 16:52:55 +0100

    [ADD] Concrete Cat and Dog classes added

---
+++ Abstraction/src/ie/sinnott/animal/Dog.java
@@ -0,0 +1,12 @@
+package ie.sinnott.animal;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override public void makeSound() {
+        System.out.println(getName() + " barks");
+    }
+}
