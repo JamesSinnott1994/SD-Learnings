eb90a6dbe16351fcb63c8fdb0cf60a7587382a3d
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-18 16:31:03 +0100

    [ADD] Concrete "Rectangle" class added

---
+++ Abstraction/src/ie/sinnott/Rectangle.java
@@ -0,0 +1,24 @@
+package ie.sinnott;
+
+public class Rectangle extends Shape {
+
+    double length;
+    double width;
+
+    public Rectangle(String colour, double length, double width) {
+        // Calling Shape constructor
+        super(colour);
+        System.out.println("Rectangle constructor called");
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override double area() {
+        return length * width;
+    }
+
+    @Override public String toString() {
+        return "Rectangle colour is " + super.getColour() + "and area is: " + area();
+    }
+
+}
