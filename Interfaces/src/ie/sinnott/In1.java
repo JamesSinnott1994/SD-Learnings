d6be4525e0ce2eaac88823d4b9c685636bbd520a
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-08-26 16:59:23 +0100

    [ADD] Simple interface example

---
+++ Interfaces/src/ie/sinnott/In1.java
@@ -0,0 +1,11 @@
+package ie.sinnott;
+
+// A simple interface
+public interface In1 {
+
+    // public, static and final
+    final int a = 10;
+
+    // public and abstract
+    void display();
+}
---
+++ Interfaces/src/ie/sinnott/TestClass.java
@@ -0,0 +1,9 @@
+package ie.sinnott;
+
+// A class that implements the interface
+public class TestClass implements In1 {
+    // Implementing the capabilities of interface
+    public void display() {
+        System.out.println("Geek");
+    }
+}
