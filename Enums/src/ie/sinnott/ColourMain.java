6c21fa8932a2407b0f1ecc4e260ac835a3d00863
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-09-03 12:34:18 +0100

    [ADD] enum with main function inside it

---
+++ Enums/src/ie/sinnott/ColourMain.java
@@ -0,0 +1,15 @@
+package ie.sinnott;
+
+public enum ColourMain {
+
+    RED,
+    GREEN,
+    BLUE;
+
+    // Driver method
+    public static void main(String[] args) {
+        ColourMain c1 = ColourMain.GREEN;
+        System.out.println(c1);
+    }
+
+}
