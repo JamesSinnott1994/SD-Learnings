b454e26974d05814f868308355ba34317d1ee1f6
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-09-12 20:04:05 +0100

    [ADD] First test added

--- HealthyCoderApp/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,7 +1,14 @@
 package com.healthycoderapp;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
+
+    @Test
+    void test () {
+        assertTrue(BMICalculator.isDietRecommended(81.2, 1.65));
+    }
 
 }
