475d5cce69cd3ddad8228df5db13721fbf59e5a3
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-09-12 20:26:25 +0100

    [ADD] DietRecommended test refactored to make it mroe clear

--- HealthyCoderApp/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -7,8 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class BMICalculatorTest {
 
     @Test
-    void test () {
-        assertTrue(BMICalculator.isDietRecommended(81.2, 1.65));
+    void Should_ReturnTrue_When_DietRecommended () {
+
+        // given
+        double weight = 81.2;
+        double height = 1.65;
+
+        // when (invoke method under test and store result in variable)
+        boolean recommonded = BMICalculator.isDietRecommended(weight, height);
+
+        // then
+        assertTrue(recommonded);
     }
 
 }
