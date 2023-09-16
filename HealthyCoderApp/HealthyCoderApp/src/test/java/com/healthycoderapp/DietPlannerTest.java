0ae5f817bee5276cb42a48cd9f4b16cae09ef097
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2023-09-16 19:44:35 +0100

    [TEST] Using @BeforeEach and @AfterEach

---
+++ HealthyCoderApp/HealthyCoderApp/src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -0,0 +1,43 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DietPlannerTest {
+
+    private DietPlanner dietPlanner;
+
+    @BeforeEach
+    public void setup() {
+        this.dietPlanner = new DietPlanner(20, 30, 50);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("A unit test was finished");
+    }
+
+    @Test
+    public void should_ReturnCorrectDietPlan_When_CorrectCoder() {
+
+        // given
+        Coder coder = new Coder(1.82, 75.0, 26, Gender.MALE);
+        DietPlan expected = new DietPlan(2202, 110, 73, 275);
+
+        // when
+        DietPlan actual = this.dietPlanner.calculateDiet(coder);
+
+        // then
+        assertAll(
+                () -> assertEquals(expected.getCalories(), actual.getCalories()),
+                () -> assertEquals(expected.getProtein(), actual.getProtein()),
+                () -> assertEquals(expected.getFat(), actual.getFat()),
+                () -> assertEquals(expected.getCarbohydrate(), actual.getCarbohydrate())
+        );
+
+    }
+
+}
